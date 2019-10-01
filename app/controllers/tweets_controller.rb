c44cc38a3fd2ea3bd6e85d9d72da0fbe380710f8
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-10-01 15:43:41 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -19,7 +19,7 @@ class TweetsController < ApplicationController
 
   post '/tweets' do
     if params[:content] != ""
-      @tweet = Tweet.create(content: params[:content], id: session[:user_id])
+      @tweet = Tweet.create(content: params[:content], user_id: session[:user_id])
     end
     redirect '/tweets'
   end
